fb507a2461d614acc36c827f954f7196d5f3b7ff
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-08-31 11:49:08 -0300

    Removed reset button (not part of what was requested)

--- app/src/main/java/com/rome/tech/ticmasproject/compare/view/MainViewModel.kt
+++ app/src/main/java/com/rome/tech/ticmasproject/compare/view/MainViewModel.kt
@@ -11,8 +11,8 @@ import com.rome.tech.ticmasproject.compare.model.EvaluateStrings
 // Business logic
 class MainViewModel : ViewModel() {
 
-//    private val result: Int = -1
-    private var _result: Int = -1
+    private val result: Int = -1
+    private var _result: Int = result
 
     val comparisonResult: LiveData<ComparisonResult> get() = _comparisonResult
     private var _comparisonResult: MutableLiveData<ComparisonResult> =
@@ -34,18 +34,18 @@ class MainViewModel : ViewModel() {
         _result = if (str1.isNotEmpty() && str2.isNotEmpty()) when (EvaluateStrings(
             str1.toString(), str2.toString()
         ).evaluate()) {
-            true -> 1  // Equal character strings
-            else -> 0  // Different character strings
+            true -> 1
+            else -> 0
         }
         else {
-            -1  // insufficient number of character strings
+            result
         }
         updateResult(_result)
     }
 
 
     fun actionReset() {
-        updateResult(-1)  // Set as insufficient number of character strings
+        updateResult(result)
     }
 
 
