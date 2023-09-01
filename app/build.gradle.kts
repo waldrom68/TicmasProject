cb1a67156ff050c2b273da22812473e38cf0b3fb
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-08-31 22:13:45 -0300

    UI: change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 22
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.3"
+        versionName = "1.0.4"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
